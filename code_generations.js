289a24a9a8962cbdc9a3221564d3120e97e9eccf
henclewood-mck <67712921+henclewood-mck@users.noreply.github.com>
2023-10-02 17:30:00 -0400

    Update code_generations.js

--- code_generations.js
+++ code_generations.js
@@ -3,6 +3,7 @@ class Solution {
         for (let i = 0; i < numbers.length; i++) {
             for (let j = i + 1; j < numbers.length; j++) {
                 let distance = numbers[i] - numbers[j];
+                let distance = numbers[i] - numbers[j];
                 if (dis tance < threshold) return true;
             }
         }
