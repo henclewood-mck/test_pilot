32063e2f66f897cf9002364e2c9c156b4bb5bc73
henclewood-mck <67712921+henclewood-mck@users.noreply.github.com>
2023-10-03 14:57:41 -0400

    Update code_generations.java
    insert "buggy code" from bigcode humanevalpack

--- code_generations.java
+++ code_generations.java
@@ -1,11 +1,9 @@
-class Solution {
-    hasCloseElements(numbers, threshold) {
-        for (let i = 0; i < numbers.length; i++) {
-            for (let j = i + 1; j < numbers.length; j++) {
-                let dista nce = numbers[i] - numbers[j];
-                if (dis tance < threshold) return true;
-            }
-        }
-        return false;
-    }
+for (int i = 0; i < numbers.size(); i++) {
+  for (int j = i + 1; j < numbers.size(); j++) {
+    double distance = numbers.get(i) - numbers.get(j);
+    if (distance < threshold) return true;
+  }
+}
+return false;
+}
 }
