c1292f80cff2316c21b6ad637a6dd3dd7ed5be30
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-03 08:17:19 -0400

    initialized repd testing

--- code_generations.js
+++ code_generations.js
@@ -2,8 +2,7 @@ class Solution {
     hasCloseElements(numbers, threshold) {
         for (let i = 0; i < numbers.length; i++) {
             for (let j = i + 1; j < numbers.length; j++) {
-                let distance = numbers[i] - numbers[j];
-                let distance = numbers[i] - numbers[j];
+                let dista nce = numbers[i] - numbers[j];
                 if (dis tance < threshold) return true;
             }
         }
