bd8cfe08af26def373d65e58ae0d761ad7698ede
henclewood-mck <67712921+henclewood-mck@users.noreply.github.com>
2023-10-03 15:09:23 -0400

    Update code_generations.java
    x + y

--- code_generations.java
+++ code_generations.java
@@ -1,10 +1,10 @@
 for (int i = 0; i < numbers.size(); i++) {
   for (int j = i + 1; j < numbers.size(); j++) {
     double distance = numbers.get(i) - numbers.get(j);
-    double distance = numbers.get(i) - numbers.get(j);
-    if (dis tance < threshold) return true;
+    if (distance < threshold) return true;
   }
 }
 return false;
 }
 }
+x + y
