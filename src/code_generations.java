360010a72a74667966a73c2529ee4d73c23bf434
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-03 17:39:52 -0400

    add if ( !(a == 2)) to test error catching

--- src/code_generations.java
+++ src/code_generations.java
@@ -2,6 +2,7 @@ for (int i = 0; i < numbers.size(); i++) {
   for (int j = i + 1; j < numbers.size(); j++) {
     double distance = numbers.get(i) - numbers.get(j);
     if (distance < threshold) return true;
+    if ( !(a == 2)) return true;
   }
 }
 return false;
