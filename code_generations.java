56319d7176f2b90e95c8bc733ddf628cbe7faec0
henclewood-mck <67712921+henclewood-mck@users.noreply.github.com>
2023-10-03 15:57:02 -0400

    Update code_generations.java

--- code_generations.java
+++ code_generations.java
@@ -1,7 +1,6 @@
 for (int i = 0; i < numbers.size(); i++) {
   for (int j = i + 1; j < numbers.size(); j++) {
-double distance = numbers.get(i) - numbers.get(j);
-    blah blah
+    double distance = numbers.get(i) - numbers.get(j)
     if (distance < threshold) return true;
   }
 }
