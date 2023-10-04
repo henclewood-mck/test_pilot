0a3802dcb18ce24c0ffb7560d6a29040b5ea2d34
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-04 14:43:33 -0400

    add bug v15

--- src/SolutionGen.java
+++ src/SolutionGen.java
@@ -6,7 +6,7 @@ public class SolutionGen {
   public boolean hasCloseElements(List < Double > numbers, double threshold) {
     for (int i = 0; i < numbers.size(); i++) {
       for (int j = i + 1; j < numbers.size(); j++) {
-        double dis tance = numbers.get(i) - numbers.get(j);
+        double distance = numbers.get(i) - numbers.get(j);
         if (distance < threshold) return true;
       }
     }
