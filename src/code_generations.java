99bb33c47515defd27d2a8942fd79eb4f1aa734b
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-03 22:10:50 -0400

    removed commented code

--- src/code_generations.java
+++ src/code_generations.java
@@ -12,18 +12,3 @@ class Solution {
     return false;
   }
 }
-
-/*for (int i = 0; i < numbers.size(); i++) {
-  for (int j = i + 1; j < numbers.size(); j++) {
-    double distance = numbers.get(i) - numbers.get(j);
-    if (distance < threshold) return true;
-    if ( !(a == 2)) {
-      return true;
-    }
-  }
-}
-return false;
-}
-x + y = 1
-}*/
-
