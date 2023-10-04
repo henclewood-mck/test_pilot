af9fa6509f0b933b079cb7ce2229d9b4d288c8aa
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-04 08:58:57 -0400

    add Ref and Gen tests

--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -2,13 +2,13 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.*;
-public class SolutionTest {
+
+public class SolutionTestGen {
     @Test
     public void test()
     {
-        Solution s = new Solution();
+        SolutionGen s = new SolutionGen();
         List < Boolean > correct = Arrays.asList(s.hasCloseElements(new ArrayList < > (Arrays.asList(11.0, 2.0, 3.9, 4.0, 5.0, 2.2)), 0.3), 
         !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 3.9, 4.0, 5.0, 2.2)), 0.05), 
         s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 5.9, 4.0, 5.0)), 0.95), 
@@ -17,7 +17,25 @@ public class SolutionTest {
         s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 1.0), 
         !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 0.5));
         if (correct.contains(false)) {
-            assert false; /*throw new AssertionError();*/
+            throw new AssertionError();
+            }
+    }
+}
+
+public class SolutionTestRef {
+    @Test
+    public void test()
+    {
+        SolutionRef s = new SolutionRef();
+        List < Boolean > correct = Arrays.asList(s.hasCloseElements(new ArrayList < > (Arrays.asList(11.0, 2.0, 3.9, 4.0, 5.0, 2.2)), 0.3), 
+        !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 3.9, 4.0, 5.0, 2.2)), 0.05), 
+        s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 5.9, 4.0, 5.0)), 0.95), 
+        !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 5.9, 4.0, 5.0)), 0.8), 
+        s.hasCloseElements(new ArrayList < > (Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 2.0)), 0.1), 
+        s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 1.0), 
+        !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 0.5));
+        if (correct.contains(false)) {
+            throw new AssertionError();
             }
     }
 }
