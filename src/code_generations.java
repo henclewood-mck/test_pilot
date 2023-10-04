fc5d4ab1a4b3436523e06a7f2e0a91df9a8e554d
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-04 09:09:09 -0400

    add bug

--- src/code_generations.java
+++ src/code_generations.java
@@ -1,12 +1,12 @@
 package src;
 import java.util.*;
 import java.lang.*;
-class Solution {
+class SolutionGen {
   public boolean hasCloseElements(List < Double > numbers, double threshold) {
     for (int i = 0; i < numbers.size(); i++) {
       for (int j = i + 1; j < numbers.size(); j++) {
         double distance = numbers.get(i) - numbers.get(j);
-        if (distance < threshold) return true;
+        if (dis tance < threshold) return true;
       }
     }
     return false;
