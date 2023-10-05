338aae806a77e074447e7060f9e79049a6017460
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-05 17:24:10 -0400

    v_10_5_23

--- test/SolutionGenTest.java
+++ test/SolutionGenTest.java
@@ -7,7 +7,7 @@ import src.SolutionGen;
 
 public class SolutionGenTest {
     @Test
-    public void test()
+    public void closeElementsGen()
     {
         SolutionGen s = new SolutionGen();
         List < Boolean > correct = Arrays.asList(s.hasCloseElements(new ArrayList < > (Arrays.asList(11.0, 2.0, 3.9, 4.0, 5.0, 2.2)), 0.3), 
@@ -18,7 +18,7 @@ public class SolutionGenTest {
         s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 1.0), 
         !s.hasCloseElements(new ArrayList < > (Arrays.asList(1.1, 2.2, 3.1, 4.1, 5.1)), 0.5));
         if (correct.contains(false)) {
-            /*throw new AssertionError();*/
+            throw new AssertionError();
             }
     }
 }
