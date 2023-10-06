25d92eb82379cee1bd9cf64039b00bdd8648e851
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-06 14:32:27 -0400

    revert to distance < threshold

--- src/SolutionRef.java
+++ src/SolutionRef.java
@@ -7,7 +7,7 @@ public class SolutionRef {
     for (int i = 0; i < numbers.size(); i++) {
       for (int j = i + 1; j < numbers.size(); j++) {
         double distance = Math.abs(numbers.get(i) - numbers.get(j));
-        if (distance > threshold) return true;
+        if (distance < threshold) return true;
       }
     }
     return false;
