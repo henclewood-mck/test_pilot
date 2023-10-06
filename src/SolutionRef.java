904a2a9903da6254f9b6e145aafcde041bf41ddc
Dwayne Henclewood <Dwayne_Henclewood@mckinsey.com>
2023-10-06 12:52:17 -0400

    add ref bug - [j = i - 1]

--- src/SolutionRef.java
+++ src/SolutionRef.java
@@ -5,9 +5,9 @@ import java.lang.*;
 public class SolutionRef {
   public boolean hasCloseElements(List < Double > numbers, double threshold) {
     for (int i = 0; i < numbers.size(); i++) {
-      for (int j = i + 1; j < numbers.size(); j++) {
+      for (int j = i - 1; j < numbers.size(); j++) {
         double distance = Math.abs(numbers.get(i) - numbers.get(j));
-        if (dis tance < threshold) return true;
+        if (distance < threshold) return true;
       }
     }
     return false;
